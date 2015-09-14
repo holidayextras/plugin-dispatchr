76d645fc3777119d4ee3c1ef4fff05ffd2b86b49
Phil Meehan <philip.meehan@gmail.com>
2015-09-14 08:36:25 +0100

    Wrap dispatchr-module with validation and promises for integration into the Works

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/* jslint node: true */
 'use strict';
 
-/*
-* @name /index.js
-* @description Node style library inclusion
-* @since Mon Jun 02 2014
-* @author Kevin Hodges <kevin.hodges@holidayextras.com>
-*/
-
-module.exports = require( './lib/pluginHttp.js' );
+module.exports = require( './lib/pluginDispatchr.js' );
--- test/pluginDispatchrTest.js
+++ test/pluginDispatchrTest.js
@@ -1,29 +1,28 @@
-/* jslint node: true */
+/*eslint no-unused-expressions:0 */
 'use strict';
 
 var chai = require( 'chai' );
 var chaiAsPromised = require( 'chai-as-promised' );
 chai.use( chaiAsPromised );
 chai.should();
+var sinon = require( 'sinon' );
+var _ = require( 'lodash' );
 
-
+var dispatchr = require( 'dispatchr-module' );
 var Hapi = require( 'hapi' );
 
 var server;
+
+// allow resources to be required safely
+function loadTestResource( resource ) {
+  return _.cloneDeep( require( './' + resource ) );
+}
 
 describe( 'pluginDispatchr', function() {
 
   before( function( done ) {
     // Need to start up a server
     server = new Hapi.Server();
-    server.methods.getConfig = function() {
-      return {
-        cache: {
-          host: '127.0.0.1',
-          port: '6379'
-        }
-      };
-    };
 
     // and then register this plugin to that server
     server.pack.register( require( '../lib/pluginDispatchr' ), function() {
@@ -31,14 +30,69 @@ describe( 'pluginDispatchr', function() {
     } );
   } );
 
-  describe( '.register()', function() {
+  describe( '#register', function() {
     it( 'should allow us to access the plugin off the hapi server', function( done ) {
       server.plugins[ 'plugin-dispatchr' ].should.not.equal( undefined );
       done();
     } );
   } );
 
-  describe( '.makeItSo()', function() {
+  describe( '#publishEmail', function() {
+
+    context( 'Validation', function() {
+
+      it( 'should fail when the options are missing required parameters', function() {
+        return server.plugins[ 'plugin-dispatchr' ].publishEmail( {} ).should.eventually.be.rejected.and.eventually.have.property( 'error' ).that.is.an.instanceof( TypeError );
+      } );
+
+    } );
+
+    context( 'Dispatchr module successful', function() {
+      var checkType;
+      var checkOptions;
+
+      before( function( done ) {
+        sinon.stub( dispatchr, 'publish', function( type, options, callback ) {
+          checkType = type;
+          checkOptions = options;
+          callback();
+        } );
+        done();
+      } );
+
+      it( 'should be call dispatchr-module with the correct parameters and resolve', function() {
+        return server.plugins[ 'plugin-dispatchr' ].publishEmail( loadTestResource( 'fixtures/validOptions' ) ).then( function() {
+          checkType.should.equal( 'email' );
+          checkOptions.should.deep.equal( loadTestResource( 'expected/validMergedOptions' ) );
+        } );
+      } );
+
+      after( function( done ) {
+        dispatchr.publish.restore();
+        done();
+      } );
+
+    } );
+
+    context( 'Dispatchr module errorred', function() {
+
+      before( function( done ) {
+        sinon.stub( dispatchr, 'publish', function( type, options, callback ) {
+          callback( 'error ');
+        } );
+        done();
+      } );
+
+      it( 'should pass back errors from dispatchr-module as a rejected promise', function() {
+        return server.plugins[ 'plugin-dispatchr' ].publishEmail( loadTestResource( 'fixtures/validOptions' ) ).should.be.rejected;
+      } );
+
+      after( function( done ) {
+        dispatchr.publish.restore();
+        done();
+      } );
+
+    } );
 
 
   } );
